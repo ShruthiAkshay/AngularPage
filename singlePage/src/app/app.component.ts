8392c54a2c3b7d3dc7bef0733b12d0f82f7f8e56
Akshay L <akshay.kalkur@gmail.com>
2018-09-21 11:26:41 +0530

    added the first page

--- singlePage/src/app/app.component.ts
+++ singlePage/src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+
+//here i am defining the app interfcace which is used to create all your apps 
+export interface App {
+  name: string;
+  desc: string;
+  links: string[];
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'singlePage';
+export class AppComponent implements OnInit{
+  // this is to validate that an app is always selected
+  appControl = new FormControl('', [Validators.required]);
+  
+  
+  // this is the array of your apps
+  apps: App[] = [
+    {name: 'LAVS',desc: 'LAVS', links: ['https://google.com','yourWebsite.com','yourHyperlink']},
+    {name: 'NCU',desc: 'NCU', links: ['https://google.com','yourWebsite.com','yourHyperlink']}
+  ];
+  selected:boolean = false;
+  
+  ngOnInit(){
+
+  }
+
+  // this is the submit function which is triggered when clicked to display the list of hyperlinks
+  onSubmit(array:string[]){
+    this.selected = true;
+    console.log(array);
+  }
+
+  //this is the function to call your service, left it blank as i have no services with me which can fecth data
+  callService(){
+    console.log('Works');
+    // write function here to call your service
+  }
 }
--- singlePage/src/app/app.module.ts
+++ singlePage/src/app/app.module.ts
@@ -2,13 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatInputModule } from '@angular/material';
+import { MatSelectModule } from '@angular/material/select';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
+    MatInputModule,
+    MatSelectModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
